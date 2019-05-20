feb1fcefd3dae7f2859f9fdfae670894763f91b8
mollyjb <50813199+mollyjb@users.noreply.github.com>
2019-05-20 15:48:02 -0400

    added things to two of them
    fine food and UnGZip

--- src/main/java/lucenelambda/FineFoodReview.java
+++ src/main/java/lucenelambda/FineFoodReview.java
@@ -6,6 +6,37 @@ package lucenelambda;
  * as the only kind of document we'll index/return as search result.
  */
 public class FineFoodReview {
+    
     // TODO add csv fields
     // Id,ProductId,UserId,ProfileName,HelpfulnessNumerator,HelpfulnessDenominator,Score,Time,Summary,Text
+    //hashmap
+    //get array list from GetObject
+    
+    HashMap<String, List<String>> food = new HashMap<String, List<Integer>>();
+    ArrayList<String> begin = new ArrayList<String>();
+    File file = new File("review.csv.txt"); 
+    Scanner sc = new Scanner(file); 
+     
+    while (sc.hasNextLine()) 
+    {
+        String str[] = sc.split(",");
+        //al = Arrays.asList(str);//other option
+        for(int i = 0; i < str.length(); i++);
+        {
+            begin.add(str[i]);
+        }
+        //can use a for loop to put split array into arraylist form
+    } 
+    for(int i = 0; i < 10; i++)
+    {
+        ArrayList<String> end = new ArrayList<String>();
+        for(int j = 0 + i; j < begin.size(); j+=10)
+        {
+            end.add(begin[j]);
+        }
+        food.put(i,end);
+    }
+    
 }
+
+
---
+++ src/main/java/lucenelambda/UnGZip.java
@@ -0,0 +1,49 @@
+// Java program to illustrate  
+// Decompressing a File using GZIPOutputStream 
+import java.io.FileInputStream; 
+import java.io.FileOutputStream; 
+import java.io.IOException; 
+import java.util.zip.GZIPInputStream; 
+  
+class UnGZip
+{ 
+    static final String INPUT_FILE  
+            = "reviews.csv.gz"; 
+    static final String OUTPUT_FILE  
+            = "reviews.csv.txt"; 
+      
+    static void decompress() 
+    { 
+        byte[] buffer = new byte[1024]; 
+        try
+        { 
+            GZIPInputStream is =  
+                    new GZIPInputStream(new FileInputStream(INPUT_FILE)); 
+                      
+            FileOutputStream out = 
+                    new FileOutputStream(OUTPUT_FILE); 
+              
+            int totalSize; 
+            while((totalSize = is.read(buffer)) > 0 ) 
+            { 
+                out.write(buffer, 0, totalSize); 
+            } 
+              
+            out.close(); 
+            is.close(); 
+              
+            System.out.println("File Successfully decompressed"); 
+        } 
+        catch (IOException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+          
+    } 
+      
+    public static void main (String[] args) 
+    { 
+        decompress(); 
+          
+    } 
+} 
