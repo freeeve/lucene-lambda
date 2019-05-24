2baf7121aaaf7f59fbe44bbd813cfd308e8a7925
mollyjb <50813199+mollyjb@users.noreply.github.com>
2019-05-24 11:23:20 -0400

    added to indexerLambda

--- src/main/java/lucenelambda/IndexerLambda.java
+++ src/main/java/lucenelambda/IndexerLambda.java
@@ -1,20 +1,82 @@
 package lucenelambda;
+package indexer;
+package org.apache.lucene.demo;//new
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import java.io.Reader;
+import java.io.FileReader;
+import java.io.BufferedReader;//new from here
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Date;//all the way to here
+
+import org.apache.lucene.analysis.Analyzer;//new from here 
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.LongPoint;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;//to here
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 
 /**
  * Lambda class for Indexing.
  */
-public class IndexerLambda implements RequestHandler<IndexerLambdaRequest, IndexerLambdaResponse> {
+public class IndexerLambda implements RequestHandler<IndexerLambdaRequest, IndexerLambdaResponse> 
+{
 
     @Override
-    public IndexerLambdaResponse handleRequest(IndexerLambdaRequest indexerLambdaRequest, Context context) {
+    public IndexerLambdaResponse handleRequest(IndexerLambdaRequest indexerLambdaRequest, Context context) 
+    {
         // TODO add logging (using log4j: https://docs.aws.amazon.com/lambda/latest/dg/java-logging.html)
         // 1. read file from s3 (https://s3.amazonaws.com/fannie-data/lucene-lambda-test/reviews.csv.gz)
         // see example: https://docs.aws.amazon.com/AmazonS3/latest/dev/RetrievingObjectUsingJava.html
         // and combine with: https://docs.oracle.com/javase/7/docs/api/java/util/zip/GZIPInputStream.html
-
+        System.out.println("test");
+		
+		private static final String INDEX_DIR = "c:review.csv";
+		Reader in = new FileReader("review.csv");
+		int count = 0;
+		
+		//Getting the elements one by one: Iterable
+		Iterable<CSVRecord> records = CSVFormat.DEFAULT.withHeader("Id", "ProductId","UserId","ProfileName","HelpfulnessNumerator","HelpfulnessDenominator","Score","Time","Summary","Text").parse(in);
+		for (CSVRecord record : records) 
+		{
+			String id = record.get("Id");
+			String productId = record.get("ProductId");
+			String userId = record.get("UserId");
+			String profileName = record.get("ProfileName");
+			String helpfulnessNumerator = record.get("HelpfulnessNumerator");
+			String helpfulnessDenominator = record.get("HelpfulnessDenominator");
+			String score = record.get("Score");
+			String time = record.get("Time");
+			String summary = record.get("Summary");
+			String text = record.get("Text");
+			count++;
+			
+	        Document document = new Document();
+	        document.add(new StringField("id", id.toString() , Field.Store.YES));
+	        document.add(new TextField("productId", productId , Field.Store.YES));
+	        document.add(new TextField("userId", userId , Field.Store.YES));
+	        document.add(new TextField("profileName", profileName , Field.Store.YES));
+	        return document;
+		}
         // 1b. parse csv
         // see reader example: https://www.baeldung.com/apache-commons-csv
 
